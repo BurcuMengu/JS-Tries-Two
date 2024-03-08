8ea389d88d01a08587b34fd85ededf79a6938d7c
Burcu Mengu <burcumengu@gmail.com>
2024-03-08 15:35:50 +0300

    push index.js

--- index.js
+++ index.js
@@ -89,3 +89,24 @@ function bmiCalculator (weight, height) {
 }
 
 bmiCalculator(80, 190);
+
+
+
+
+function isLeap(year) {
+
+    if (year % 4 === 0) {
+        if (year% 100 === 0 ) {
+            if (year % 400 ===0) {
+                return "leap year";
+    } else {
+        return "not leap year";
+    }
+        }    else {
+        return "leap year.";
+        }
+    } else {
+        return "not leap year.";
+    }
+}
+isLeap(2400);
